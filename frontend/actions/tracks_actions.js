302942def5133d3997165a675b0c9ed43900a07e
App Academy Student <student@appacademy.io>
2016-11-04 09:30:03 -0700

    adds track actions

---
+++ frontend/actions/tracks_actions.js
@@ -0,0 +1,47 @@
+export const FETCH_TRACKS = "FETCH_TRACKS";
+export const FETCH_TRACK = "FETCH_TRACK";
+export const CREATE_TRACK = "CREATE_TRACK";
+export const UPDATE_TRACK = "UPDATE_TRACK";
+export const DELETE_TRACKS = "DELETE_TRACK";
+export const RECEIVE_TRACKS = "RECEIVE_TRACKS";
+export const RECEIVE_TRACK = "RECEIVE_TRACK";
+export const REMOVE_TRACK = "REMOVE_TRACK";
+
+export const fetchTracks = () => ({
+  type: FETCH_TRACKS
+});
+
+export const fetchTrack = id => ({
+  type: FETCH_TRACK,
+  id
+});
+
+export const createTrack = track => ({
+  type: CREATE_TRACK,
+  track
+});
+
+export const updateTrack = track => ({
+  type: UPDATE_TRACK,
+  track
+});
+
+export const deleteTrack = id => ({
+  type: DELETE_TRACK,
+  id
+});
+
+export const receiveTracks = tracks => ({
+  type: RECEIVE_TRACKS,
+  tracks
+});
+
+export const receiveTrack = track => ({
+  type: RECEIVE_TRACK,
+  track
+});
+
+export const removeTrack = track => ({
+  type: REMOVE_TRACK,
+  track
+});
